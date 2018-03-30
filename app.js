217bdf223409e2c5950fa496bc074b76423ec67b
Matheus Rodrigues <matheusstudenty@gmail.com>
2018-03-30 19:17:46 -0300

    Aula 70 fim

--- app.js
+++ app.js
@@ -2,9 +2,48 @@
 var app = require('./config/server');
 
 /* parametrizar a porta de escuta */
-app.listen(80, function() {
+var server = app.listen(80, function() {
     console.log('Servidor ON');
 })
 
+var io = require('socket.io').listen(server);
 
-//parei na pagin 13 aula 65
+app.set('io', io);
+
+/* criar a conexao por websocket */
+io.on('connection', socket => {
+    
+    console.log('Usuario conectou');
+    
+    socket.on('disconnect', () =>{
+        console.log('Usuario desconectou');
+    })
+
+    socket.on('msgParaServidor', data => {
+        
+        /* dialogo */
+        socket.emit(
+            'msgParaCliente', 
+            { apelido: data.apelido, mensagem: data.mensagem } 
+        );
+
+        socket.broadcast.emit(
+            'msgParaCliente', 
+            { apelido: data.apelido, mensagem: data.mensagem }
+        );
+
+        /* participante */
+        if(parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+            socket.emit(
+                'participantesParaCliente', 
+                { apelido: data.apelido } 
+            );
+
+            socket.broadcast.emit(
+                'participantesParaCliente', 
+                { apelido: data.apelido }
+            );
+        }
+    })
+})
+//parei na pagin 13 aula 70
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -11,5 +11,10 @@ module.exports.iniciaChat = function(application, res, req){
         return;
     }
 
-    res.render('chat');
+    application.get('io')
+        .emit('msgParaCliente', 
+              {apelido: dadosForm.apelido, mensagem: 'Acabou de entrar no chat' }
+        );
+
+    res.render('chat', { dadosForm: dadosForm });
 }
